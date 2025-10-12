06b4a78cf5456c93cc79229d7ce9ecfb0beb0fcd
rhit-lucasja <lucasja@rose-hulman.edu>
2025-10-11 21:32:29 -0400

    try-catch for GET and logs 10 random questions

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
-async function test() {// jService - Trivia API API Example
-    const response = await fetch('https://quiz-game-backend-5gpd.onrender.com/', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
 
-    console.log(`OpenTDB API response has code ${response.response_code}`);
-    console.log(response);
+/**
+ * A simple function for testing that I can properly get data from API
+ * 
+ * This function routes through Node backend and gets 10 random questions from any category.
+ * It prints the raw JSON data to the console log.
+ * 
+ */
+async function test() {// jService - Trivia API API Example
+    try {
+        const response = await fetch('https://quiz-game-backend-5gpd.onrender.com', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json(); // extract JSON data
+        console.log(data);
+    } catch (error) {
+        console.error("Error fetching 10 random questions");
+    }
 }
 
 test();
