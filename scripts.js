e3eb1df9389dc3aaa873f513ee75c7bb0cc29c1b
rhit-lucasja <lucasja@rose-hulman.edu>
2025-10-11 21:36:09 -0400

    renamed API testing function to better reflect purpose

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 /**
  * A simple function for testing that I can properly get data from API
  * 
@@ -6,7 +5,7 @@
  * It prints the raw JSON data to the console log.
  * 
  */
-async function test() {// jService - Trivia API API Example
+async function printRandom10() {// jService - Trivia API API Example
     try {
         const response = await fetch('https://quiz-game-backend-5gpd.onrender.com', {
             method: 'GET',
@@ -20,5 +19,3 @@ async function test() {// jService - Trivia API API Example
         console.error("Error fetching 10 random questions");
     }
 }
-
-test();
