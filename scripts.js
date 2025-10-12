0a90c5788cf0260bf49f6937747e0e469916a39e
rhit-lucasja <lucasja@rose-hulman.edu>
2025-10-11 21:57:55 -0400

    front end testing buttons and printAllCategories() function

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@
  * It prints the raw JSON data to the console log.
  * 
  */
-async function printRandom10() {// jService - Trivia API API Example
+async function printRandom10() {
     try {
         const response = await fetch('https://quiz-game-backend-5gpd.onrender.com', {
             method: 'GET',
@@ -18,4 +18,39 @@ async function printRandom10() {// jService - Trivia API API Example
     } catch (error) {
         console.error("Error fetching 10 random questions");
     }
+}
+
+
+/**
+ * Retrieves the names and corresponding IDs of all categories in OpenTDB.
+ * 
+ * Prints the category info to the console log.
+ * 
+ */
+async function printAllCategories() {
+    try {
+        const response = await fetch('https://quiz-game-backend-5gpd.onrender.com/allCategories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        // take data and iterate through to print each category and its ID
+        for (const entry of data.trivia_categories) {
+            console.log(`Category: ${entry.name} \tID: ${entry.id}`);
+        }
+    } catch (error) {
+        console.error("Error fetching category list");
+    }
+}
+
+
+/**
+ * Given a category ID, prints to the console log the number of questions in that category.
+ * 
+ * @param {int} id The ID of the category whose size is being requested
+ */
+async function printNumInCategory(id) {
+    
 }
