9f579c18e933b8b5e43aaf0735b6fc596767671c
rhit-lucasja <lucasja@rose-hulman.edu>
2025-10-13 13:48:55 -0400

    made so that any non-integer catID passed in defaults to taking a quiz on any category

--- quizScripts.js
+++ quizScripts.js
@@ -6,12 +6,12 @@ function getCategoryID() {
     // get the URL search parameter for category ID as string
     const params = new URLSearchParams(window.location.search);
     const strID = params.get("catID");
-    if (!strID || strID === "any") {
+    const catID = parseInt(strID);
+    if (Number.isNaN(catID)) {
         // argument was not provided - so default to any category works
         console.log("You decided to start a quiz on any category.");
     } else {
         // parse argument strID as an integer catID
-        const catID = parseInt(strID);
         console.log(`You started a quiz on category with catID ${catID}.`);
     }
 
